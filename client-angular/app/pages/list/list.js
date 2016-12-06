0e9c56f5d2103ada7a3cf80e20f1cbf484e634f9
Emre Vatansever <vatanseveremre90@gmail.com>
2016-12-06 10:43:11 +0300

    Notify service edited

--- client-angular/app/pages/list/list.js
+++ client-angular/app/pages/list/list.js
@@ -1,11 +1,13 @@
 angular
 	.module('GaarajCardVisit')
-	.controller('listController', function (Card, Utils, $state, Notify ) {
+	.controller('listController', function (Card, Utils, $state, Notify) {
 		var vm = this;
 
 		function getcardList(argument) {
 			Card.index().success(function (response) {
-				vm.card = response.data;
+				vm.card = response.data.filter(function (item) {
+					return item.status === "Active";
+				});
 			}).error(function () {
 				Notify.success('Kart listesi alınamadı');
 			});
@@ -18,19 +20,24 @@ angular
 		vm.addCard = addCard;
 
 		function cardDelete(card_id) {
-			Card.delete(card_id)
-				.success(function (response) {
-					Notify.success(response.message);
-					getcardList();
-				})
-				.error(function (error) {
-					Notify.success(error.message);
-				});
+			Notify.confirm(function (confİtem) {
+				if (confİtem) {
+					Card.delete(card_id)
+						.success(function (response) {
+							Notify.success(response.message);
+							getcardList();
+						})
+						.error(function (error) {
+							Notify.success(error.message);
+						});
+				}
+			}, function (error) {});
+
 		}
 		vm.cardDelete = cardDelete;
 
 		function cardEdit(card_id) {
-			$state.go('app.editCard', { card_id : card_id });
+			$state.go('app.editCard', { card_id: card_id });
 		}
 		vm.cardEdit = cardEdit;
 	});
